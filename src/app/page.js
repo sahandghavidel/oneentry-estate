aec0f4b7414e1aa6bc13cc2c2309b596934f0d26
Sahand Ghavidel <sahand.gha@gmail.com>
2024-06-30 21:08:44 +0700

    Complete the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,103 @@
-export default function Home() {
-  return <div className='text-red-600'>Home</div>;
+import ListingItem from '@/components/ListingItem';
+import Link from 'next/link';
+import { defineOneEntry } from 'oneentry';
+
+const { Products } = defineOneEntry('https://sahandestate.oneentry.cloud', {
+  token: process.env.NEXT_PUBLIC_ONEENTRY_TOKEN,
+});
+
+export default async function Home() {
+  const rentListings = await Products.getProductsByPageUrl('rent');
+  const saleListings = await Products.getProductsByPageUrl('sell');
+  const AllListings = await Products.getProductsByPageUrl('listings');
+  return (
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-slate-500'>perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          Sahand Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link
+          href={'/listings'}
+          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+        >
+          Let&apos;s get started...
+        </Link>
+      </div>
+      <img
+        src='https://firebasestorage.googleapis.com/v0/b/mern-auth-1c4ae.appspot.com/o/1693307829089home%203.jpeg?alt=media&token=8dcc9a22-a8d3-4737-b27f-7c77b417a7d0'
+        className='w-full h-[550px] object-cover'
+      />
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {AllListings && AllListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent Listings
+              </h2>
+              <Link
+                className='text-sm text-blue-800 hover:underline'
+                href={'/listings'}
+              >
+                Show more listings
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {AllListings.map((listing) => (
+                <ListingItem listing={listing} key={listing.id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {rentListings && rentListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent places for rent
+              </h2>
+              <Link
+                className='text-sm text-blue-800 hover:underline'
+                href={'/rent'}
+              >
+                Show more places for rent
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {rentListings.map((listing) => (
+                <ListingItem listing={listing} key={listing.id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {saleListings && saleListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent places for sale
+              </h2>
+              <Link
+                className='text-sm text-blue-800 hover:underline'
+                href={'/sell'}
+              >
+                Show more places for sale
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {saleListings.map((listing) => (
+                <ListingItem listing={listing} key={listing.id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
